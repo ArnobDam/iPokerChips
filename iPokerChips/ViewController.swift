e01e62e06754f2b1b046b2d2fc34e3dfbb226cbb
Nicholas Deily <deily@wustl.edu>
2019-11-11 19:03:16 -0600

    new changes

--- iPokerChips/ViewController.swift
+++ iPokerChips/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBAction func addPlayer(_ sender: Any) {
+    }
+    
+    
     @IBAction func playPressed(_ sender: Any) {
     }
     @IBOutlet weak var playButton: UIButton!
@@ -19,7 +25,12 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        
+        
+        playButton.layer.cornerRadius = 10
+        playButton.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 
