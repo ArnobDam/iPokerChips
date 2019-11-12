c9b0b667e58e417150308e7e1a6ede65f907183b
Billy Habimana Cyusa <bhabimanacyusa@wustl.edu>
2019-11-11 19:30:21 -0600

    Implementation of picker view

--- iPokerChips/ViewController.swift
+++ iPokerChips/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    var pickerData = ["2", "3","4","5","6","7","8","9", "10"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+       return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+
+    
 
     @IBOutlet weak var addButton: UIButton!
     
@@ -18,6 +35,8 @@ class ViewController: UIViewController {
     
     @IBAction func playPressed(_ sender: Any) {
     }
+    @IBOutlet weak var playerDropdown: UIPickerView!
+    @IBOutlet weak var selectPlayers: UILabel!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var scrollView: UIScrollView!
     
@@ -28,10 +47,14 @@ class ViewController: UIViewController {
 
         super.viewDidLoad()
         
-        
+//setting the Play button's corners round
         playButton.layer.cornerRadius = 10
         playButton.clipsToBounds = true
-        // Do any additional setup after loading the view.
+    
+        
+//Set the delegate and data source to self
+        self.playerDropdown.delegate = self
+        self.playerDropdown.dataSource = self
     }
 
 
