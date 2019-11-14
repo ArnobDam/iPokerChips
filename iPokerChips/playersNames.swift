2484e79d19457baf8126fe8938f8548e838f22ab
ndeily123 <deily@wustl.edu>
2019-11-13 18:54:21 -0600

    updates

--- iPokerChips/playersNames.swift
+++ iPokerChips/playersNames.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class playersNames: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class playersNames: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     var numberOfPlayers: Int?
+    var names:[String] = []
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -21,17 +22,56 @@ class playersNames: UIViewController, UITableViewDelegate, UITableViewDataSource
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCell(withIdentifier: "textFieldCell") as! textFieldTableViewCell
+        var frameRect = cell.textField.frame
+        frameRect.size.height = 60 // <-- Specify the height you want here.
+        cell.textField.frame = frameRect
+        cell.textField.placeholder = String(format: "Player %d", Int(indexPath.row+1))
+        cell.textField.font = UIFont(descriptor: .init(), size: 21)
+        cell.textField.delegate = self
+        cell.textField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+
         return cell
     }
     
     
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        let index = playerTableView.indexPath(for: textField.superview?.superview as! UITableViewCell)
+        
+        names[(index?.row)!] = textField.text!
+    }
+    
+ 
     
     @IBOutlet weak var playButton: UIButton!
+    @IBAction func playPressed(_ sender: Any) {
+        
+        for (index, name) in names.enumerated() {
+            if name.trimmingCharacters(in: .whitespaces) == "" {
+                
+                names[index] = String(format: "Player %d", Int(index+1))
+            }
+            
+            
+            
+            
+        }
+        
+        
+        print(names)
+    }
+    
     @IBOutlet weak var playerTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = false
-
+        playerTableView.dataSource = self
+        playerTableView.delegate = self
+        playerTableView.rowHeight = 74.0
+        
+        for i in 1...numberOfPlayers! {
+            names.append(String(format:"Player %d", i))
+        }
+        
         // Do any additional setup after loading the view.
     }
     
