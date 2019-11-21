381fc2e3e43bab3023a180afe5002d5802b852ae
Billy Habimana Cyusa <bhabimanacyusa@wustl.edu>
2019-11-20 18:17:11 -0600

    created view

---
+++ iPokerChips/GameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameViewController.swift
+//  iPokerChips
+//
+//  Created by Billy Habimana Cyusa on 11/20/19.
+//  Copyright © 2019 Nicholas Deily. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    var handSize: Int?
+    var playerNames: [String] = []
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iPokerChips/playerContentView.swift
@@ -0,0 +1,22 @@
+//
+//  playerContentView.swift
+//  iPokerChips
+//
+//  Created by Billy Habimana Cyusa on 11/20/19.
+//  Copyright © 2019 Nicholas Deily. All rights reserved.
+//
+
+import UIKit
+
+class playerContentView: UIView {
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+    }
+ 
+
+}
